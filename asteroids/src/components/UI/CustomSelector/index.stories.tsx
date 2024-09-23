c4a610a79f6427a7ce4fabb9a16e5ab3c115ba6c
arshcherbakov <shcherbakov.fortech@gmail.com>
2024-09-24 01:08:11 +0300

    feature: template for selector

--- asteroids/src/components/UI/CustomSelector/index.stories.tsx
+++ asteroids/src/components/UI/CustomSelector/index.stories.tsx
@@ -1,18 +1,49 @@
+import { useState } from 'react';
+import { SelectChangeEvent, Box } from '@mui/material';
+import { Meta, StoryFn } from '@storybook/react';
 import CustomSelector from '.';
+import ICustomSelectorProps from './interface';
 
-const arr = {
-  km: 'kilometrs',
-  m: 'milies',
+const listForSelector = {
+  kilometers: 'Километры',
+  meters: 'Метры',
+  miles: 'Мили',
+  feet: 'Фут',
 };
+
 export default {
-  title: 'CustomSelector',
+  title: 'UI/CustomSelector',
   component: CustomSelector,
+} as Meta<typeof CustomSelector>;
+
+const Template: StoryFn<ICustomSelectorProps> = args => {
+  const [selelctorValue, setSelectorValue] = useState('kilometers');
+
+  const handleSelector = (event: SelectChangeEvent<unknown>) => {
+    const newValue = event.target.value as keyof typeof listForSelector;
+    setSelectorValue(newValue);
+  };
+
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <CustomSelector
+        {...args}
+        selelctorValue={selelctorValue}
+        handleSelector={handleSelector}
+      />
+    </Box>
+  );
 };
 
-export const Default = () => (
-  <CustomSelector
-    dataList={arr}
-    handleSelector={() => {}}
-    selelctorValue="papa"
-  />
-);
+export const Default = Template.bind({});
+Default.args = {
+  dataList: listForSelector,
+  handleSelector: (event: SelectChangeEvent<unknown>) => {},
+};
